cda6ae13ee4043b08b09870abbc49060eb79e2f8
vunam5066 <90817945+vunam5066@users.noreply.github.com>
2021-09-20 12:56:13 +0700

    hihi

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'Hà Khùng',
+    introDesc: `Haha Tui giỡn thui á `,
+    btnIntro: 'Hihi bên trong vẫn chưa có gì ',
+    title: 'title',
+    desc: 'desc',
+    btnYes: 'btnYes',
+    btnNo: 'btnNo',
+    question:'question',
+    btnReply: 'btnReply',
+    reply: 'reply',
+    mess: 'mess',
+    messDesc: 'messDesc',
+    btnAccept: 'btnAccept',
+    messLink: 'messLink', //link fb
+    bgLink: 'https://huypham9205.github.io/tang-crush/img/bg.webp', //link ảnh nền
+    iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
+    lookMeLink: 'https://huypham9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
+    soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
